a4cef06010419962af3f0da88e011759f6b1014f
Abyalew <AWondimgegn@excellerentsolutions.com>
2022-04-25 11:34:51 +0300

    moved entities to models folder;

---
+++ CMS.DataModel/Repositories/Reposotory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataModel.Repositories
+{
+    class Reposotory
+    {
+        public Reposotory()
+        {
+            //_context = new 
+        }
+    }
+}
