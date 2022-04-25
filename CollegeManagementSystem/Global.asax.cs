ea61361671e2bdb641b992881902f39764fed7f7
Abyalew <AWondimgegn@excellerentsolutions.com>
2022-04-25 12:54:37 +0300

    added api configuration

--- CollegeManagementSystem/Global.asax.cs
+++ CollegeManagementSystem/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +11,7 @@ namespace CollegeManagementSystem
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
