f7528345827f7b37486bc7b505b14c2cbbec816b
Abyalew <AWondimgegn@excellerentsolutions.com>
2022-04-27 14:02:43 +0300

    removed registration number form student table; added migration

---
+++ CMS.DataModel/Migrations/202204271021390_RemoveRegistrationNumberFromStudentTable.cs
@@ -0,0 +1,18 @@
+﻿namespace CMS.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveRegistrationNumberFromStudentTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Students", "RegistrationNumber");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "RegistrationNumber", c => c.String());
+        }
+    }
+}
--- CMS.DataModel/Models/Student.cs
+++ CMS.DataModel/Models/Student.cs
@@ -1,9 +1,15 @@
-﻿namespace CMS.DataModel
+﻿using System.Collections.Generic;
+
+namespace CMS.DataModel
 {
     public class Student
     {
+        public Student()
+        {
+            Addmitions = new List<Addmition>();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
-        public string RegistrationNumber { get; set; }
+        public ICollection<Addmition> Addmitions { get; set; }
     }
 }
